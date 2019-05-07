b03eb2bcfb3669fad030869f1a3ee5ec05ba91d4
krman1 <mar.aniol@interia.pl>
2019-05-07 22:21:54 +0200

    getTask

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,6 +1,9 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,26 +15,33 @@ import java.util.List;
 @RestController
 @RequestMapping ("/v1/task")
 public class TaskController {
+	@Autowired
+	private DbService service;
+	@Autowired
+	private TaskMapper taskMapper;
+
+	@RequestMapping(method = RequestMethod.GET, value = "getTasks")
+	public List<TaskDto> getTasks(){
+		return taskMapper.mapToTaskDtoList(service.getAllTasks());
+	}
 	
 	@RequestMapping(method = RequestMethod.GET, value = "getTask")
-	public List<TaskDto> getTask(){
-		return new ArrayList<>();
+	public TaskDto getTask (Long taskId) {
+		return taskMapper.mapToTaskDto(service.getTask(taskId).get());
 	}
-	@RequestMapping(method = RequestMethod.GET, value = "getTask/{taskId}")
-	public TaskDto getTask (@PathVariable Long taskId){
-		return new TaskDto(1L,"test title", "test_content");
-	}
-	@RequestMapping (method = RequestMethod.DELETE, value = "deleteTask/{taskId}")
-	public void deleteTask (@PathVariable Long taskId) {
 	
+	@RequestMapping (method = RequestMethod.DELETE, value = "deleteTask")
+	public void deleteTask (Long taskId) {
 	}
-	@RequestMapping (method = RequestMethod.PUT, value = "updateTask/{taskDto}")
-	public TaskDto updateTask (@PathVariable TaskDto taskDto){
+	
+	@RequestMapping (method = RequestMethod.PUT, value = "updateTask")
+	public TaskDto updateTask (TaskDto taskDto){
 		return new TaskDto(1L,"edited test title", "Test content");
 	}
-	@RequestMapping (method = RequestMethod.POST, value = "createTask/{taskDto}")
-	public void createTask (@PathVariable TaskDto taskDto) {
 	
+	@RequestMapping (method = RequestMethod.POST, value = "createTask")
+	public void createTask (TaskDto taskDto) {
+
 	}
 }
 
--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -3,12 +3,44 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Getter
-@AllArgsConstructor
+import javax.persistence.*;
+
+//@Getter
+//@NoArgsConstructor
+//@AllArgsConstructor
+@Entity (name = "tasks")
 public class Task {
-	
+	@Id
+	@GeneratedValue (strategy = GenerationType.AUTO)
 	private Long id;
+	
+	@Column (name = "name")
 	private String title;
+	
+	@Column (name = "description")
 	private String content;
+	
+	public Task(){
+	}
+	
+	public Task(Long id,String title, String content) {
+		this.id = id;
+		this.title = title;
+		this.content = content;
+		
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getContent() {
+		return content;
+	}
 }
